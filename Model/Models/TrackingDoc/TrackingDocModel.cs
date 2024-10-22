036f4592d9b3375c1a2f1a305e2d038823176cd6
Dharam-k-Singh <dharamkumar.singh@edgeaxis.in>
2024-10-22 19:13:47 +0530

    Tracking Document Initiated

---
+++ Model/Models/TrackingDoc/TrackingDocModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models.TrackingDoc
+{
+    public class TrackingDocModel
+    {
+        public int TrackDocId { get; set; }
+        public string SenderOrgName { get; set; }
+        public int DocType { get; set; }
+        public string Subject { get; set; }
+        public string DocSummary { get; set; }
+        public string RecipientMailId { get; set; }
+        public string DetailedDescription { get; set; }
+        public int DocumentStatus { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0: yyyy-MM-dd}")]
+        public DateTime ReceivedDate { get; set; }
+        public int RequiredAction { get; set; }
+        public int RequiredAttention { get; set; }
+        [IgnoreDataMember]
+        [Required(ErrorMessage = "At least One Document is Required")]
+        public HttpPostedFileBase AttachDoc { get; set; }
+        public int ChangedBy { get; set; }
+        public int IsActive { get; set; }
+    }
+}
---
+++ WEB/Controllers/TrackDocumentController.cs
@@ -0,0 +1,40 @@
+﻿using Model.Models.TrackingDoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Controllers
+{
+    public class TrackDocumentController : BaseController
+    {
+        private void GetMaster()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TrackDoc()
+        {
+            return View("TrackDocument");
+        }
+        [HttpPost]
+        public ActionResult TrackDoc(TrackingDocModel model)
+        {
+            return RedirectToAction("");
+        }
+        public ActionResult List()
+        {
+            List<TrackingDocModel> lst = new List<TrackingDocModel>();
+            return View("DocTrackingList", lst);
+        }
+        public void UpdateTracking()
+        {
+
+        }
+    }
+}
